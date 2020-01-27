8c562ba95a26c11bad98ae48968e7defc8361e63
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:54:38 +0100

    Add project files.

---
+++ EsercizioStruttureDati 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioStruttureDati_2
+{
+    class Program
+    {
+        private const string file = @"file.txt";
+        private const string file1 = @"file1.txt";
+
+        static void Main(string[] args)
+        {
+            HashSet<int> numeri = new HashSet<int>();
+            using (StreamReader rfile = new StreamReader(file))
+            {
+                string line;
+                while ((line = rfile.ReadLine()) != null)
+                {
+                 int n = int.Parse(line);
+                 numeri.Add(n);
+                }
+            }
+            using (StreamWriter swfile = new StreamWriter(file1, false))
+            {
+             foreach (int d in numeri)
+             swfile.WriteLine(d);
+            }
+        }
+    }
+}
